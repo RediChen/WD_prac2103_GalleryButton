8dfbfd636b38fb3fd1258f853881a06145e8c775
RediiChen <a0929052958@gmail.com>
2021-03-09 13:43:13 +0800

    機制建立進度（60%）

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ var slideIndex = 1 ;
 
 //函數宣告區 ===================
 function showSlides(n) {
-    var i ;
+    var i ;//for 迴圈用的這個變數不會被關在for裡頭，而會被提昇到這裡。
     var slides = document.getElementsByClassName("slides") ;
     var dots = document.getElementsByClassName("demo") ;
     var showText = document.getElementById("caption") ;
-
+    // 處理超出範圍的值
     if (n > slides.length) {
         slideIndex = 1 ;
     } else if (n < 1) {
-        
+        n = slides.length ;
     }
+    // 全員隱藏
+    for (i = 0 ; i < dots.length ; i++) {
+        slides[i].style.display = "none" ;
+    }
+    // 點點模式切換
 }
 
 function plusSlides(k) {
