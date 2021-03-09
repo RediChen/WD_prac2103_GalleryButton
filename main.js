1e06fe52d21cc13a046119f379d5a3175c876d71
RediiChen <a0929052958@gmail.com>
2021-03-09 22:39:02 +0800

    建立機制(100%)＆調整版型

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var slideIndex = 1 ;
+var slideIndex = 0 ;//count from 0
+showSlides(slideIndex) ;
 
 //函數宣告區 ===================
 function showSlides(n) {
@@ -7,21 +8,31 @@ function showSlides(n) {
     var dots = document.getElementsByClassName("demo") ;
     var showText = document.getElementById("caption") ;
     // 處理超出範圍的值
-    if (n > slides.length) {
-        slideIndex = 1 ;
-    } else if (n < 1) {
-        n = slides.length ;
+    if (n >= slides.length) {
+        slideIndex = 0 ;
+    } else if (n < 0) {
+        slideIndex = slides.length - 1 ;
     }
-    // 全員隱藏
+    // 各組全員隱藏
     for (i = 0 ; i < dots.length ; i++) {
         slides[i].style.display = "none" ;
     }
-    // 點點模式切換
+    // 所有點點還原
+    for (i = 0 ; i < dots.length ; i++) {
+        dots[i].className = dots[i].className.replace(" active", "") ;
+        // 為什麼 active 要有空格是因為：只會作字串的串接，必須連空格一起給。
+    }
+    // 主角圖組出現
+    slides[slideIndex].style.display = "block" ;
+    // 主角點點啟動
+    dots[slideIndex].className += " active" ;
+    // 給予主角的資訊
+    showText.innerHTML = dots[slideIndex].alt ;
 }
 
 function plusSlides(k) {
     showSlides(slideIndex += k) ;
 }
-function currenSlides(n) {
+function currentSlide(n) {
     showSlides(slideIndex = n) ;
 }
